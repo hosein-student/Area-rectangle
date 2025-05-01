607f1583338b8627cf6dff4803c9d60ca556ca5d
gjvng <hoseinjaf84ri84@gmail.com>
2025-05-01 17:36:58 +0330

    استفاده از رویداد فرم لود

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -91,6 +91,7 @@
             this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
