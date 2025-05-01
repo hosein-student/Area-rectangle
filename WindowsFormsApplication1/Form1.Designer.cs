11b4e207547ad8f9066d728a08e719f591778296
gjvng <hoseinjaf84ri84@gmail.com>
2025-05-01 19:50:27 +0330

    درست کردن نامگذاری ها

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -29,44 +29,44 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_area = new System.Windows.Forms.Button();
+            this.txt_width = new System.Windows.Forms.TextBox();
+            this.txt_height = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // button1
+            // btn_area
             // 
-            this.button1.Location = new System.Drawing.Point(135, 86);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(232, 94);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btn_area.Location = new System.Drawing.Point(135, 86);
+            this.btn_area.Name = "btn_area";
+            this.btn_area.Size = new System.Drawing.Size(232, 94);
+            this.btn_area.TabIndex = 0;
+            this.btn_area.Text = "Calculate Area";
+            this.btn_area.UseVisualStyleBackColor = true;
+            this.btn_area.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // txt_width
             // 
-            this.textBox1.Location = new System.Drawing.Point(172, 198);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(146, 34);
-            this.textBox1.TabIndex = 1;
+            this.txt_width.Location = new System.Drawing.Point(172, 198);
+            this.txt_width.Name = "txt_width";
+            this.txt_width.Size = new System.Drawing.Size(146, 29);
+            this.txt_width.TabIndex = 1;
             // 
-            // textBox2
+            // txt_height
             // 
-            this.textBox2.Location = new System.Drawing.Point(172, 238);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(146, 34);
-            this.textBox2.TabIndex = 1;
+            this.txt_height.Location = new System.Drawing.Point(172, 238);
+            this.txt_height.Name = "txt_height";
+            this.txt_height.Size = new System.Drawing.Size(146, 29);
+            this.txt_height.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(74, 204);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 29);
+            this.label1.Size = new System.Drawing.Size(54, 24);
             this.label1.TabIndex = 2;
             this.label1.Text = "width";
             // 
@@ -75,20 +75,20 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(74, 241);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 29);
+            this.label2.Size = new System.Drawing.Size(62, 24);
             this.label2.TabIndex = 2;
             this.label2.Text = "height";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(494, 459);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_height);
+            this.Controls.Add(this.txt_width);
+            this.Controls.Add(this.btn_area);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "Form1";
@@ -101,9 +101,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_area;
+        private System.Windows.Forms.TextBox txt_width;
+        private System.Windows.Forms.TextBox txt_height;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Timer timer1;
